e8d74190c7e42a853b264ae96a4396d4cace713d
LiodonLiu <122708125+LiodonLiu@users.noreply.github.com>
2023-03-02 15:55:49 +0800

    add button

--- app_api.py
+++ app_api.py
@@ -3,8 +3,8 @@ from location import locate
 from PIL import Image
 
 input_text = st.text_input('input text:', 'An ice cream near the door')
-
-locate(input_text)
-
-image = Image.open('final_canvas.jpg')
-st.image(image, caption='final_canvas')
+submit_button = st.form_submit_button('Submit')
+if submit_button:
+  locate(input_text)
+  image = Image.open('final_canvas.jpg')
+  st.image(image, caption='final_canvas')
