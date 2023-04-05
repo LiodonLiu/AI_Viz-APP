0a356853ff5d30aff479d3998ab439ccafb93f07
LiodonLiu <122708125+LiodonLiu@users.noreply.github.com>
2023-04-05 13:17:23 +0800

    Update app_api.py

--- app_api.py
+++ app_api.py
@@ -111,7 +111,7 @@ def main():
       cursor: not-allowed;pointer-events: none;color: rgba(0,0,0,0);
       }
     #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.css-1y4p8pa.egzxvld4 > div:nth-child(1) > div > div:nth-child(3) > div > div > div > img{
-      position: absolute;top: 0px;left: 0px;z-index: 2;
+      position: absolute;top: 0px;right: 0px;z-index: 2;
       }
     </style>''', unsafe_allow_html=True)
 
